71c5495c90a35cc8e81799af6197659cc7c98d15
Mark <markcfleming1990@gmail.com>
2022-04-26 18:34:53 -0400

    working on JS script

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+var cities [];
+
+var cityFormEl=document.querySelector("#city-search-form");
+var cityInputEl=document.querySelector("#city");
+var weatherContainerEl=document.querySelector("#current-weather-container");
+var citySearchInputEl = document.querySelector("#searched-city");
+var forecastTitle = document.querySelector("#forecast");
+var forecastContainerEl = document.querySelector("#fiveday-container");
+var pastSearchButtonEl = document.querySelector("#past-search-buttons");
+
+var formSumbitHandler = function(event){
+  event.preventDefault();
+  var city = cityInputEl.value.trim();
+  if(city){
+      getCityWeather(city);
+      get5Day(city);
+      cities.unshift({city});
+      cityInputEl.value = "";
+  } else{
+      alert("Please enter a City");
+  }
+  saveSearch();
+  pastSearch(city);
+}
